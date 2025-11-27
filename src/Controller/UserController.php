1267c478e9a5e12f0443f9b73dad0362d8a27b34
nolansio <nolanarsac105@gmail.com>
2025-11-27 10:45:55 +0100

    Regroupement des routes dans /api/users avec 2 paramètre facultatif id et username et ajout d'erreur correspondant

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -6,6 +6,7 @@ use App\Repository\UserRepository;
 use App\Service\JsonConverter;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use OpenApi\Attributes as OA;
@@ -23,16 +24,30 @@ class UserController extends AbstractController {
     #[Route('/api/users', methods: ['GET'])]
     #[OA\Get(
         path: '/api/users',
-        summary: 'Récupère tous les utilisateurs',
-        description: 'Récupération de tous les utilisateur',
+        summary: "Récupère tous les utilisateurs ou un utilisateur par son ID ou nom d'utilisateur",
+        description: "Récupération de tous les utilisateurs ou un utilisateur par son par son ID ou nom d'utilisateur",
         tags: ['Utilisateur'],
+        parameters: [
+            new OA\Parameter(
+                name: "id",
+                in: "query",
+                required: false,
+                schema: new OA\Schema(type: "integer")
+            ),
+            new OA\Parameter(
+                name: "username",
+                in: "query",
+                required: false,
+                schema: new OA\Schema(type: "string")
+            )
+        ],
         responses: [
             new OA\Response(
                 response: 200,
-                description: 'Utilisateurs récupérés avec succès',
+                description: 'Utilisateur/s récupéré/s avec succès',
                 content: new OA\JsonContent(
                     properties: [
-                        new OA\Property(property: 'id', type: 'int', example: 1),
+                        new OA\Property(property: 'id', type: 'integer', example: 1),
                         new OA\Property(property: 'username', type: 'string', example: 'user'),
                         new OA\Property(property: 'user_identifier', type: 'string', example: 'user'),
                         new OA\Property(property: 'roles', type: 'array', items: new OA\Items(type: 'object'), example: ['ROLE_USER']),
@@ -41,6 +56,15 @@ class UserController extends AbstractController {
                         new OA\Property(property: 'dislikes', type: 'array', items: new OA\Items(type: 'object'), example: []),
                         new OA\Property(property: 'posts', type: 'array', items: new OA\Items(type: 'object'), example: []),
                         new OA\Property(property: 'comments', type: 'array', items: new OA\Items(type: 'object'), example: [])
+                    ]
+                )
+            ),
+            new OA\Response(
+                response: 400,
+                description: 'Mauvaise requête',
+                content: new OA\JsonContent(
+                    properties: [
+                        new OA\Property(property: 'error', type: 'string', example: "Can't use 2 parameters. Only 1 or 0 is allowed")
                     ]
                 )
             ),
@@ -55,118 +79,29 @@ class UserController extends AbstractController {
             )
         ]
     )]
-    public function getAll(): Response {
-        $users = $this->userRepository->findAll();
+    public function get(Request $request): Response {
+        $id = $request->query->get('id');
+        $username = $request->query->get('username');
+        $data = null;
 
-        return new Response($this->jsonConverter->encodeToJson($users), 200, ['Content-Type' => 'application/json']);
-    }
+        if ($id && $username) {
+            return new JsonResponse(['error' => "Can't use 2 parameters. Only 1 or 0 is allowed"], 400);
+        }
 
-    #[Route('/api/users/{id}', methods: ['GET'])]
-    #[OA\Get(
-        path: '/api/users/{id}',
-        summary: 'Récupère un utilisateur par son ID',
-        description: "Récupération d'un utilisateur par son ID",
-        tags: ['Utilisateur'],
-        responses: [
-            new OA\Response(
-                response: 200,
-                description: 'Utilisateur récupéré avec succès',
-                content: new OA\JsonContent(
-                    properties: [
-                        new OA\Property(property: 'id', type: 'int', example: 1),
-                        new OA\Property(property: 'username', type: 'string', example: 'user'),
-                        new OA\Property(property: 'user_identifier', type: 'string', example: 'user'),
-                        new OA\Property(property: 'roles', type: 'array', items: new OA\Items(type: 'object'), example: ['ROLE_USER']),
-                        new OA\Property(property: 'password', type: 'string', example: '$2y$13$IZVb2Y5dGZmk...'),
-                        new OA\Property(property: 'likes', type: 'array', items: new OA\Items(type: 'object'), example: []),
-                        new OA\Property(property: 'dislikes', type: 'array', items: new OA\Items(type: 'object'), example: []),
-                        new OA\Property(property: 'posts', type: 'array', items: new OA\Items(type: 'object'), example: []),
-                        new OA\Property(property: 'comments', type: 'array', items: new OA\Items(type: 'object'), example: [])
-                    ]
-                )
-            ),
-            new OA\Response(
-                response: 401,
-                description: 'Non autorisé',
-                content: new OA\JsonContent(
-                    properties: [
-                        new OA\Property(property: 'error', type: 'string', example: 'Missing token / Invalid token')
-                    ]
-                )
-            ),
-            new OA\Response(
-                response: 404,
-                description: 'Utilisateur non trouvé',
-                content: new OA\JsonContent(
-                    properties: [
-                        new OA\Property(property: 'error', type: 'string', example: 'User not found')
-                    ]
-                )
-            )
-        ]
-    )]
-    public function getById($id): JsonResponse {
-        $user = $this->userRepository->find($id);
-
-        if (!$user) {
+        if (!$id && !$username) {
+            $data = $this->userRepository->findAll();
+        }
+        elseif ($id && !$username) {
+            $data = $this->userRepository->find($id);
+        }
+        elseif (!$id && $username) {
+            $data = $this->userRepository->findOneByUsername($username);
+        }
+        elseif (!$data) {
             return new JsonResponse(['error' => 'User not found'], 404);
         }
 
-        return new JsonResponse($this->jsonConverter->encodeToJson($user), 200, [], true);
-    }
-
-    #[Route('/api/users/username/{username}', methods: ['GET'])]
-    #[OA\Get(
-        path: '/api/users/username/{username}',
-        summary: "Récupère un utilisateur par son nom nom d'utilisateur",
-        description: "Récupération d'un utilisateur par son nom d'utilisateur",
-        tags: ['Utilisateur'],
-        responses: [
-            new OA\Response(
-                response: 200,
-                description: 'Utilisateur récupéré avec succès',
-                content: new OA\JsonContent(
-                    properties: [
-                        new OA\Property(property: 'id', type: 'int', example: 1),
-                        new OA\Property(property: 'username', type: 'string', example: 'user'),
-                        new OA\Property(property: 'user_identifier', type: 'string', example: 'user'),
-                        new OA\Property(property: 'roles', type: 'array', items: new OA\Items(type: 'object'), example: ['ROLE_USER']),
-                        new OA\Property(property: 'password', type: 'string', example: '$2y$13$IZVb2Y5dGZmk...'),
-                        new OA\Property(property: 'likes', type: 'array', items: new OA\Items(type: 'object'), example: []),
-                        new OA\Property(property: 'dislikes', type: 'array', items: new OA\Items(type: 'object'), example: []),
-                        new OA\Property(property: 'posts', type: 'array', items: new OA\Items(type: 'object'), example: []),
-                        new OA\Property(property: 'comments', type: 'array', items: new OA\Items(type: 'object'), example: [])
-                    ]
-                )
-            ),
-            new OA\Response(
-                response: 401,
-                description: 'Non autorisé',
-                content: new OA\JsonContent(
-                    properties: [
-                        new OA\Property(property: 'error', type: 'string', example: 'Missing token / Invalid token')
-                    ]
-                )
-            ),
-            new OA\Response(
-                response: 404,
-                description: 'Utilisateur non trouvé',
-                content: new OA\JsonContent(
-                    properties: [
-                        new OA\Property(property: 'error', type: 'string', example: 'User not found')
-                    ]
-                )
-            )
-        ]
-    )]
-    public function getByUsername($username): JsonResponse {
-        $user = $this->userRepository->findOneByUsername($username);
-
-        if (!$user) {
-            return new JsonResponse(['error' => 'User not found'], 404);
-        }
-
-        return new JsonResponse($this->jsonConverter->encodeToJson($user), 200, [], true);
+        return new JsonResponse($this->jsonConverter->encodeToJson($data), 200, [], true);
     }
 
 }
